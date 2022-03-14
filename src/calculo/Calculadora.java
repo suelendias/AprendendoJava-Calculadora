f5460442a22d498464f3d4de10b59f6cf282ed3c
suelendias <diassuelen01@gmail.com>
2022-03-14 17:09:01 -0300

    Primeiro commit

---
+++ src/calculo/Calculadora.java
@@ -0,0 +1,26 @@
+package calculo;
+
+public class Calculadora 
+{
+
+	public static double soma(double numero1, double numero2) 
+    {
+        return numero1 + numero2;        
+    }
+
+	public static double subtracao(double numero1, double numero2) 
+    {
+		return numero1 - numero2;
+    }
+
+	public static double multiplicacao(double numero1, double numero2) 
+    {
+		return numero1 * numero2;        
+    }
+
+	public static double  divisao(double numero1, double numero2) 
+    {
+		return numero1 / numero2;
+	}
+
+}
---
+++ src/calculo/Main.java
@@ -0,0 +1,80 @@
+package calculo;
+
+import java.util.Scanner;
+
+
+public class Main 
+{
+
+    public static void main(String[] args) 
+    {      	
+		int opcao = 5;
+		double numero1;
+		double numero2;
+		
+	    Scanner input = new Scanner(System.in); 
+    	
+    	System.out.println("******CALCULADORA******");
+    	System.out.println("-Escolha uma op��o-");
+	    System.out.println("1. Soma");  
+	    System.out.println("2. Subtra��o");  
+	    System.out.println("3. Multiplica��o");  
+	    System.out.println("4. Divis�o");	    
+	    
+	    opcao = input.nextInt();   
+	    
+	    switch (opcao) 
+	    {
+	    	case 1:
+	    	opcao = 1;
+	    	Scanner input1 = new Scanner(System.in);
+	    	System.out.println("Qual o primeiro numero: ");
+	    	numero1 = input1.nextInt();
+	    	System.out.println("Qual o segundo numero: ");
+	    	numero2 = input1.nextInt();		
+			double resultado1 = Calculadora.soma(numero1, numero2);			
+			System.out.println("O resultado �: " + resultado1);
+			break;
+			
+	    	case 2:
+		    opcao = 2;
+		    Scanner input2 = new Scanner(System.in);
+		    System.out.println("Qual o primeiro numero: ");
+		    numero1 = input2.nextInt();
+		    System.out.println("Qual o segundo numero: ");
+		    numero2 = input2.nextInt();		
+			double resultado2 = Calculadora.subtracao(numero1, numero2);			
+			System.out.println("O resultado �: " + resultado2);
+			break;
+			
+	    	case 3:
+			opcao = 3;
+			Scanner input3 = new Scanner(System.in);
+			System.out.println("Qual o primeiro numero: ");
+			numero1 = input3.nextInt();
+			System.out.println("Qual o segundo numero: ");
+			numero2 = input3.nextInt();		
+			double resultado3 = Calculadora.multiplicacao(numero1, numero2);			
+			System.out.println("O resultado �: " + resultado3);
+			break;
+			
+	    	case 4:
+			opcao = 4;
+			Scanner input4 = new Scanner(System.in);
+			System.out.println("Qual o primeiro numero: ");
+			numero1 = input4.nextInt();
+			System.out.println("Qual o segundo numero: ");
+			numero2 = input4.nextInt();		
+			double resultado4 = Calculadora.divisao(numero1, numero2);			
+			System.out.println("O resultado �: " + resultado4);
+			break;
+	    
+	    	default:
+	    	System.out.println("Erro");
+	    }
+
+	  
+	    input.close();
+
+    }
+}
