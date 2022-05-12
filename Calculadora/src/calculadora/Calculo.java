491371478ee3adf74c7381632bc9e705c0b67486
Suelen Dias <60116445+suelendias@users.noreply.github.com>
2022-05-12 13:47:54 -0300

    Add files via upload

---
+++ Calculadora/src/calculadora/Calculo.java
@@ -0,0 +1,23 @@
+package calculadora;
+
+public class Calculo {
+    public static double soma(double numero1, double numero2)
+    {
+        return numero1 + numero2;
+    }
+
+    public static double subtracao(double numero1, double numero2)
+    {
+        return numero1 - numero2;
+    }
+
+    public static double multiplicacao(double numero1, double numero2)
+    {
+        return numero1 * numero2;
+    }
+
+    public static double  divisao(double numero1, double numero2)
+    {
+        return numero1 / numero2;
+    }
+}
